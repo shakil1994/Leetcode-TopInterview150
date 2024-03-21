08070e215fbcd38333c3b8f774560325315103ce
android_shakil <shakil!@#$1234>
2024-03-21 17:24:59 +0600

    1. Two Sum

---
+++ Hashmap/1. Two Sum.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
+            }
+            map[nums[i]] = i
+        }
+        throw IllegalArgumentException("No two sum solution")
+    }
+}
